60f0458b9ab6906b7f38460c3d130b44a139632c
Ethan Brews <ethanbrews77@gmail.com>
2017-05-03 20:54:14 +0100

    Fixed Commands

--- src/main/java/net/shadowmage/ancientwarfare/core/command/CommandResearch.java
+++ src/main/java/net/shadowmage/ancientwarfare/core/command/CommandResearch.java
@@ -1,104 +1,91 @@
 package net.shadowmage.ancientwarfare.core.command;
 
-import java.util.List;
-
-import net.minecraft.command.ICommand;
+import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.command.WrongUsageException;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.TextComponentTranslation;
 import net.shadowmage.ancientwarfare.core.research.ResearchGoal;
 import net.shadowmage.ancientwarfare.core.research.ResearchTracker;
 
-public class CommandResearch implements ICommand
-{
+/**
+ * Created by ethan on 03/05/2017.
+ */
+public class CommandResearch extends CommandBase {
 
-
-private int permissionLevel = 2;
-
-public int compareTo(ICommand par1ICommand)
-  {
-  return this.getCommandName().compareTo(par1ICommand.getCommandName());
-  }
-
-@Override
-public int compareTo(Object par1Obj)
-  {
-  return this.compareTo((ICommand)par1Obj);
-  }
-
-@Override
-public String getCommandName()
-  {
-  return "awresearch";
-  }
-
-@Override
-public String getCommandUsage(ICommandSender p_71518_1_)
-  {
-  return "command.aw.research.usage";
-  }
-
-@SuppressWarnings("rawtypes")
-@Override
-public List getCommandAliases()
-  {
-  return null;
-  }
-
-@Override
-public void processCommand(ICommandSender sender, String[] commandParts)
-  {
-  if(commandParts ==null || commandParts.length<2){throw new WrongUsageException("command.aw.research.usage", new Object[]{});}
-  String operation = commandParts[0];
-  String target = commandParts[1];
-  if(operation.equals("add"))
-    {
-    if(commandParts.length<3){throw new WrongUsageException("command.aw.research.usage", new Object[]{});}
-    String goal = commandParts[2];
-    if(!goal.startsWith("research.")){goal = "research."+goal;}
-    ResearchGoal rGoal = ResearchGoal.getGoal(goal);
-    if(rGoal==null){throw new WrongUsageException("command.aw.research.usage", new Object[]{});}
-    ResearchTracker.instance().addResearchFromNotes(sender.getEntityWorld(), target, rGoal.getId());
+    @Override
+    public String getCommandName() {
+        return "awresearch";
     }
-  else if(operation.equals("remove"))
-    {
-    if(commandParts.length<3){throw new WrongUsageException("command.aw.research.usage", new Object[]{});}
-    String goal = commandParts[2];
-    if(!goal.startsWith("research.")){goal = "research."+goal;}
-    ResearchGoal rGoal = ResearchGoal.getGoal(goal);
-    if(rGoal==null){throw new WrongUsageException("command.aw.research.usage", new Object[]{});}
-    ResearchTracker.instance().removeResearch(sender.getEntityWorld(), target, rGoal.getId());
-    }
-  else if(operation.equals("fill"))
-    {
-    ResearchTracker.instance().fillResearch(sender.getEntityWorld(), target);
-    }
-  else if(operation.equals("clear"))
-    {
-    ResearchTracker.instance().clearResearch(sender.getEntityWorld(), target);
-    }
-  else
-    {
-    throw new WrongUsageException("command.aw.research.usage", new Object[]{});
-    }
-  }
 
-@Override
-public boolean canCommandSenderUseCommand(ICommandSender par1ICommandSender)
-  {
-  return par1ICommandSender.canCommandSenderUseCommand(this.permissionLevel, this.getCommandName());
-  }
+    @Override
+    public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
+        return server.isSinglePlayer() || super.checkPermission(server, sender);
+    }
 
-@SuppressWarnings("rawtypes")
-@Override
-public List addTabCompletionOptions(ICommandSender sender, String[] p_71516_2_)
-  {
-  return null;
-  }
+    @Override
+    public String getCommandUsage(ICommandSender var1)
+    {
+        return "command.aw.research.usage";
+    }
 
-@Override
-public boolean isUsernameIndex(String[] var1, int var2)
-  {
-  return var2==1;//e.g. /awresearch add shadowmage45
-  }
+    @Override
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) {
+
+        if(args ==null || args.length<2){
+            invalidCommandUsage(sender);
+            return;
+        }
+        String operation = args[0];
+        String target = args[1];
+        if(operation.equals("add"))
+        {
+            if(args.length<3){
+                invalidCommandUsage(sender);
+                return;
+            }
+            String goal = args[2];
+            if(!goal.startsWith("research.")){goal = "research."+goal;}
+            ResearchGoal rGoal = ResearchGoal.getGoal(goal);
+            if(rGoal==null){
+                invalidCommandUsage(sender);
+                return;
+            }
+            ResearchTracker.instance().addResearchFromNotes(sender.getEntityWorld(), target, rGoal.getId());
+        }
+        else if(operation.equals("remove"))
+        {
+            if(args.length<3){
+                invalidCommandUsage(sender);
+                return;
+            }
+            String goal = args[2];
+            if(!goal.startsWith("research.")){goal = "research."+goal;}
+            ResearchGoal rGoal = ResearchGoal.getGoal(goal);
+            if(rGoal==null){
+                invalidCommandUsage(sender);
+                return;
+            }
+            ResearchTracker.instance().removeResearch(sender.getEntityWorld(), target, rGoal.getId());
+        }
+        else if(operation.equals("fill"))
+        {
+            ResearchTracker.instance().fillResearch(sender.getEntityWorld(), target);
+        }
+        else if(operation.equals("clear"))
+        {
+            ResearchTracker.instance().clearResearch(sender.getEntityWorld(), target);
+        }
+        else
+        {
+            invalidCommandUsage(sender);
+            return;
+        }
+        
+    }
+
+    public void invalidCommandUsage(ICommandSender sender) {
+        sender.addChatMessage(new TextComponentTranslation("command.aw.research.usage"));
+    }
 
 }
